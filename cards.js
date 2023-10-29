cad5f08151fa7a487d2697c3f72dc20f56c61285
VitorLaert <128254481+VitorLaert@users.noreply.github.com>
2023-10-28 23:08:44 -0400

    BETA 1

--- cards.js
+++ cards.js
@@ -1,16 +1,15 @@
 document.getElementById('closeModal').addEventListener('click', function () {
-    var modal = document.getElementById('myModal');
-    var overlay = document.getElementById('modalOverlay');
-  
-    modal.classList.remove('active');
-    overlay.style.display = 'none';
-  });
-  
-  document.querySelector('.button').addEventListener('click', function () {
-    var modal = document.getElementById('myModal');
-    var overlay = document.getElementById('modalOverlay');
-  
-    modal.classList.add('active');
-    overlay.style.display = 'block';
-  });
-  
+  var modal = document.getElementById('myModal');
+  var overlay = document.getElementById('modalOverlay');
+
+  modal.classList.remove('active');
+  overlay.style.display = 'none';
+});
+
+document.querySelector('.button').addEventListener('click', function () {
+  var modal = document.getElementById('myModal');
+  var overlay = document.getElementById('modalOverlay');
+
+  modal.classList.add('active');
+  overlay.style.display = 'block';
+});
